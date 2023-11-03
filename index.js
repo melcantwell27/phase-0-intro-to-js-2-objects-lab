8d753121bcb2ee32be72a8c5ccf9fc867f6e0d83
Melanie <melican@LAPTOP-GKKKJDUA.>
2023-11-03 10:42:02 -0400

    committed lab trick

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = { name: "Steve", streetAddress: "around"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value; 
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const DeletedEmployeeObject = {...employee}
+    delete DeletedEmployeeObject[key];
+    return DeletedEmployeeObject; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
